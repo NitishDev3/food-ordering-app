ff35745a21405164ab70146d32a9cab7210aed92
Nitish <nitishsahunisi3@gmail.com>
2024-08-24 00:36:58 +0530

    use state hook of react

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData"
+import { useState } from "react";
 
 const Body = () => {
+    const [resListRender, setResListRender] = useState(resList);
+    const topRatedRes = () =>{
+        let filteredList = resListRender.filter((res) =>res.info.avgRating > 4.2);
+        setResListRender(filteredList);
+    }
     return (
         <div className="body" >
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn"
+                    onClick={() => {topRatedRes()}}
+                >
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className="res-constainer">
                 {/* <RestaurantCard resData={resList[0]} /> */}
-                {resList.map(res =>
+                {resListRender.map(res =>
                     <RestaurantCard key={res.info.id} resData={res} />
                 )}
             </div>
